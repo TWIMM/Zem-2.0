58def9992b77db6ca3de8b86efc475ac28186a1d
Twimm <hicli0001@gmail.com>
2024-07-03 08:23:32 +0200

    updated gradle

--- lib/Utils/UtilsClass.dart
+++ lib/Utils/UtilsClass.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:agri_market/Commons/custom_text.dart';
-import 'package:geolocator/geolocator.dart';
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -14,43 +13,6 @@ class UtilsClass {
 
     return input.replaceRange(
         keepVisible, input.length, '*' * (input.length - keepVisible));
-  }
-
-  static Future<Position> determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
   }
 
   static void showActivityPickerBottomSheet(
